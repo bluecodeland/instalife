350b6296c41c46d1fc5e94eb3dd7bc300825d9d2
bluecodeland <mhtaeb@gmail.com>
2019-11-03 18:18:46 +0330

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container mt-5">
+    <div class="row">
+        <div class="col-3 p-1"><img src="/image/avatar.png" alt="" class="w-50 rounded-circle ml-5"></div>
+        <div class="col-9">
+            <div class="pt-3">
+                <h1>ChangeYourLife</h1>
+            </div>
+            <div class="d-flex">
+                <div class="pr-3"> <strong>153</strong> posts</div>
+                <div class="pr-3"> <strong>23k</strong> followers</div>
+                <div class="pr-3"> <strong>212k</strong> followeing</div>
+            </div>
+            <div class="pt-4 font-weight-bold">Changeyourlife.ir</div>
+            <div>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea enim quod doloribus deleniti expedita perferendis molestiae sunt quibusdam dicta nostrum voluptatum obcaecati, ipsam quidem. At quia culpa ipsa incidunt fugit.</div>
+            <div><a href="#">Changeyourlife.ir</a></div>
+
+            </div>
+            <div class="row pt-3">
+                <div class="col-md">
+                     <img src="image/001.jpg" alt="" class="w-100 p-2">
+                </div>
+                <div class="col-md">
+                    <img src="image/002.jpg" alt="" class="w-100 p-2">
+                </div>
+                <div class="col-md">
+                    <img src="image/001.jpg" alt="" class="w-100 p-2">
+                </div>
+            </div>
+    </div>
+</div>
+@endsection
